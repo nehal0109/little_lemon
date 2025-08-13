4386ef8ebdaf2aac1b5188544a5a6bc4c5ff7a05
unknown <Nehal jain>
2025-08-13 18:47:47 +0530

    Completed

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <section>
+        {/*banner texts*/}
+        <div className='banner'>
+          <h2>Little Lemon</h2>
+          <h3>New Delhi</h3>
+          <p>We are a family owned Mediterraneran restaurant, focused on traditional recipes served 
+            with a modern twist</p>
+        </div>
+      </section>
+    </header>
+  )
+}
+
+export default Header
